496af828724087ff3ccdbb7ea89019fe170e2c12
JB0527 <qhdrl527@naver.com>
2024-05-07 00:16:17 +0900

    dd

--- app/android/app/src/main/kotlin/com/sooming/mingle/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.sooming.mingle
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- app/mingle/index.js
+++
@@ -1,7 +0,0 @@
-const functions = require("firebase-functions");
-
-exports.helloWorld = functions.region("asia-northeast3").https.onRequest(
-    (request, response) => {
-      response.send("Hello from Firebase!");
-    },
-);
